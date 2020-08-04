8d2a2071e68807ee2019481fd576e5ae068a453c
AnnaWijetunga <annawijetunga@gmail.com>
2020-08-04 14:49:26 -0400

    Set variables to hold our data

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('Here\'s a hidden message');
+
+// hold the date and time
+// this holds reference to the value I assigned
+let today = new Date();
+let formatDate = today.toDateString();
+
+// saving the result of reformatting the date
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formateDate;
