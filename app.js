46c4e2b1efafdaa5978c1e73d0c8194d4dae89f7
AnnaWijetunga <annawijetunga@gmail.com>
2020-08-08 18:57:13 -0400

    Build timer function

--- app.js
+++ app.js
@@ -19,5 +19,22 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-
-}
+    // clear any existing timers
+    clearInterval(countdown);
+  
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+  
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we should stop it
+      if(secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+      }
+      // display the time left over
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+  }
