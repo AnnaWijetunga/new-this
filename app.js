5247ec30fbbef27e56894a6a2aa750bbbf89758d
AnnaWijetunga <annawijetunga@gmail.com>
2020-08-08 18:57:56 -0400

    Build function to display how much time is left

--- app.js
+++ app.js
@@ -38,3 +38,12 @@ function timer(seconds) {
       displayTimeLeft(secondsLeft);
     }, 1000);
   }
+
+  function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    // template literals
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+  }
